e4e915c5865031bd4b10dfc147dadc0442fcdfaa
hyuntae <believe4279@gmail.com>
2022-03-09 12:56:24 +0900

    retry

--- 14889_스타트와링트.js
+++ 14889_스타트와링트.js
@@ -1,58 +1,104 @@
-let input = '8\n0 5 4 5 4 5 4 5\n4 0 5 1 2 3 4 5\n9 8 0 1 2 3 1 2\n9 9 9 0 9 9 9 9\n1 1 1 1 0 1 1 1\n8 7 6 5 4 0 3 2\n9 1 9 1 9 1 0 9\n6 5 4 3 2 1 9 0'.toString().trim().split('\n').map(v => v.split(' ').map(Number));
-let N = input.shift();
+// let input = '4\n0 1 2 3\n4 0 5 6\n7 1 0 2\n3 4 5 0'.toString().trim().split('\n').map(v => v.split(' ').map(Number));
+// let N = input.shift();
 
-//조합
-let combiArr = [];
+// //조합
+// let combiArr = [];
 
-function combi1(curNum,array){
-    if (array.length == N/2){
-        combiArr.push(array.slice());
-        return ;
-    }
-    for (let i=curNum; i<N; i++){
-        array.push(i);
-        combi1(i+1,array);
-        array.pop();
-    }
-}
-combi1(0,[]);
+// function combi1(curNum,array){
+//     if (array.length == N/2){
+//         combiArr.push(array.slice());
+//         return ;
+//     }
+//     for (let i=curNum; i<N; i++){
+//         array.push(i);
+//         combi1(i+1,array);
+//         array.pop();
+//     }
+// }
+// combi1(0,[]);
+// let abilty = [];
 
-let abilty = [];
+// combiArr.forEach(v => {
+//     let arr = v; 
 
-combiArr.forEach(v => {
-    console.log(v);
-    let arr = v; 
+//     let answer = [];
+//     let ret = [];
 
-    let answer = [];
-    let ret = [];
+//     function combi(index,start){
+//         if(index == 2){
+//             answer.push(ret.slice());
+//             return ;
+//         }
 
-    function combi(index,start){
-        if(index == 2){
-            answer.push(ret.slice());
-            return ;
+//         for(let i=start; i<v.length; i++){
+//             ret[index] = arr[i];
+//             combi(index+1,i+1);
+//         }
+//     }
+//     combi(0,0)
+//     let totalAbilty = 0;
+
+//     answer.forEach((v) => {
+//         totalAbilty += input[v[0]][v[1]] + input[v[1]][v[0]];
+//     })
+//     abilty.push(totalAbilty);
+// })
+// //능력치 중 차이가 가장 작은 값 계산
+// let min = 999999999999999;
+// console.log(abilty);
+// for (let i=0; i<abilty.length / 2 ; i++){
+//     let different = Math.abs(abilty[i]-abilty[abilty.length-1-i]);
+//     min = Math.min(min,different);
+// }
+
+// console.log(min);
+
+// let fs = require('fs');
+
+let memberAmount = 0, min = Number.MAX_VALUE;
+let map = [], teamA = [], teamB = [];
+
+const getScore = (map, team) => {
+    let score = 0;
+    for (let i = 0; i < team.length; i++) {
+        for (let j = 0; j < team.length; j++) {
+            score += map[team[i]][team[j]];
         }
-
-        for(let i=start; i<v.length; i++){
-            ret[index] = arr[i];
-            combi(index+1,i+1);
-        }
     }
-    combi(0,0)
-    
-    let totalAbilty = 0;
-
-    answer.forEach((v) => {
-        totalAbilty += input[v[0]][v[1]] + input[v[1]][v[0]];
-    })
-    abilty.push(totalAbilty);
-})
-//능력치 중 차이가 가장 작은 값 계산
-let min = 999999999;
-abilty.sort((a,b) => a-b);
-for (let i=0; i<abilty.length / 2 - 1; i++){
-    if (abilty[i] == abilty[abilty.length-1-i]) console.log(i);
-    let different = Math.abs(abilty[i]-abilty[abilty.length-1-i]);
-    if (different < min) min = different; 
+    return score;
 }
 
-console.log(min);
+const go = (id) => {
+    if (id == memberAmount) {
+        
+        if (teamA.length != memberAmount / 2) return;
+
+        const scoreA = getScore(map, teamA);
+        const scoreB = getScore(map, teamB);
+        const diff = Math.abs(scoreA - scoreB);
+        min = Math.min(min, diff);
+        return;
+    }
+
+    teamA.push(id);
+    go(id + 1);
+    teamA.pop();
+
+    teamB.push(id);
+    go(id + 1);
+    teamB.pop();
+}
+
+const solution = (input) => {
+    memberAmount = Number(input[0]);
+    for (let i = 1; i < input.length; i++) {
+        map.push(input[i].split(' ').map((v) => Number(v)));
+    }
+
+    go(0);
+    return min;
+}
+
+const input = '4\n0 1 2 3\n4 0 5 6\n7 1 0 2\n3 4 5 0'.toString().trim().split('\n');
+const result = solution(input);
+console.log(result);
--- inflearn/k번째큰수.js
+++ inflearn/k번째큰수.js
@@ -5,30 +5,38 @@ function solution(N,K,array){
     
     let combiArray = [];
     let tmp = [];
+    let test = 0;
     function combi(start, index){
         if(index === 3){
             combiArray.push(tmp.slice());
+            console.log(combiArray)
+            test++;
             return ;
         }
-
+//array = [1,2,3]
         for(let i=start; i<array.length; i++){
             tmp[index] = array[i];
             combi(i+1, index+1);
         }
     }
+    
     combi(0,0);
-
+    console.log(test);
     let answer = new Set();
     for(let item of combiArray){
         let sum = item.reduce((acc,v) => acc + v , 0);
         answer.add(sum);
     }
-    
+
     return ([...answer].sort((a,b)=>b-a)[K-1]);
-    
-    
 
 }
 
 let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 console.log(solution(10, 3, arr));
+let arr1 = [];
+let arr2= [2,3]
+arr1.push(arr2);
+arr2[0]= 4;
+console.log(arr1);
+
