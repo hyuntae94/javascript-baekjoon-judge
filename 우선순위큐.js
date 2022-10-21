ee2d2a399cb2d0ed8aec45996048cdededb373d3
hyuntae <believe4279@gmail.com>
2022-10-21 19:50:48 +0900

    우선순위큐

--- 우선순위큐.js
+++ 우선순위큐.js
@@ -1,15 +1,84 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
   enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this.sort();
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUP();
+  }
+  bubbleUP() {
+    let idx = this.values.length - 1;
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      if (this.values[idx].priority >= this.values[parentIdx].priority) break;
+      [this.values[idx], this.values[parentIdx]] = [
+        this.values[parentIdx],
+        this.values[idx],
+      ];
+      idx = parentIdx;
+    }
   }
   dequeue() {
-    return this.values.shift();
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      const leftChildIdx = 2 * idx + 1;
+      const rightChildIdx = 2 * idx + 2;
+
+      let leftChild = null;
+      let rightChild = null;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        //최소이진힙이라 최대힙과는 달리 부등호 방향이 반대고 비교값은 priority프로퍼티이다.
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 }
+
+const pq = new PriorityQueue();
+
+pq.enqueue(10, 5);
+pq.enqueue(10, 6);
+pq.enqueue(10, 7);
+pq.enqueue(10, 2);
+pq.enqueue(10, 3);
+
+console.log(pq.dequeue());
+console.log(pq.values);
