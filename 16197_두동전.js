a2bb1fb62a3bffb8e75e991c3c00c0209e7c0c2d
hyuntae <believe4279@gmail.com>
2023-01-18 16:56:52 +0900

    16197_두동전 clear

--- 16197_두동전.js
+++ 16197_두동전.js
@@ -1,136 +1,101 @@
-const [[N,M],...input] =`5 3
-###
-.o.
-#.#
-.o.
-###`.toString().split('\n').map((el,idx) => !idx ? el.split(' ').map(Number) : el.split(''));
+const [[N, M], ...matrix] = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el, idx) => (idx === 0 ? el.split(" ").map(Number) : el.split("")));
+const initalPos = [];
 
-const COIN1 = 0;
-const COIN2 = 1;
-
-const coins = [];
-
-for(let i=0; i<N; i++){
-    for(let j=0; j<M; j++){
-        if (input[i][j] === 'o') {
-            coins.push({row:i, col:j})
-        }
+matrix.forEach((row, rI) => {
+  row.forEach((_, cI) => {
+    if (matrix[rI][cI] === "o") {
+      initalPos.push([rI, cI]);
+      matrix[rI][cI] = ".";
     }
-}
+  });
+});
+initalPos.push(0);
+const queue = [initalPos];
+const map = new Map();
 
-function check(){
+const row = [-1, 0, 1, 0];
+const col = [0, 1, 0, -1];
+let idx = 0;
+while (idx < queue.length) {
+  const [[r1, c1], [r2, c2], step] = queue[idx++];
 
-    let coin1 = false;
-    let coin2 = false;
+  //종료조건
+  //1. 10번초과
+  if (step > 10) {
+    console.log(-1);
+    return;
+  }
+  if (map.has(`${r1}${c1}${r2}${c2}`)) continue;
+  else map.set(`${r1}${c1}${r2}${c2}`, true);
 
-    if (!(coins[COIN1].row >= 0 && 
-        coins[COIN1].col >= 0 && 
-        coins[COIN1].row < N && 
-        coins[COIN1].col < M)){
-            coin1 = true;
-        }
-
-    if (!(coins[COIN2].row >= 0 && 
-        coins[COIN2].col >= 0 && 
-        coins[COIN2].row < N && 
-        coins[COIN2].col < M)){
-            coin2 = true;
-        }
-
-    if ( (coin1 == true && coin2 == false) || (coin1 == false && coin2 == true)) return true;
-    
-    return false;
-}
-
-const answer = [];
-
-function dfs(direction,count){
-
-    if (count > 10) return ;
-    if (check()) {
-        answer.push(count);
-        return ;
+  for (let i = 0; i < 4; i++) {
+    const [nextR1, nextC1, nextR2, nextC2] = [
+      r1 + row[i],
+      c1 + col[i],
+      r2 + row[i],
+      c2 + col[i],
+    ];
+    //2. 1번 동전만 밖에있는경우
+    if (
+      (nextR1 < 0 || nextR1 >= N || nextC1 < 0 || nextC1 >= M) &&
+      nextR2 >= 0 &&
+      nextR2 < N &&
+      nextC2 >= 0 &&
+      nextC2 < M
+    ) {
+      console.log(step + 1);
+      return;
     }
-  
-    let canMove1 = false;
-    let canMove2 = false;
-
-    switch (direction) {
-        case 'up':
-            const [up1X, up1Y] = [coins[COIN1].row - 1, coins[COIN1].col ]; 
-            const [up2X, up2Y] = [coins[COIN2].row - 1, coins[COIN2].col ]; 
-            if (input[up1X][up1Y] !== '#') {
-                canMove1 = true;
-                coins[COIN1].row -= 1;
-            }
-            if (input[up2X][up2Y] !== '#') {
-                canMove2 = true;
-                coins[COIN2].row -= 1;
-            }
-            break;
-        case 'down':
-            const [down1X, down1Y] = [coins[COIN1].row + 1, coins[COIN1].col ]; 
-            const [down2X, down2Y] = [coins[COIN2].row + 1, coins[COIN2].col ]; 
-            if (input[down1X][down1Y] !== '#') {
-                canMove1 = true;
-                coins[COIN1].row += 1;
-            }
-            if (input[down2X][down2Y] !== '#') {
-                canMove2 = true;
-                coins[COIN2].row += 1;
-            }
-            break;
-        case 'left':
-            const [left1X, left1Y] = [coins[COIN1].row, coins[COIN1].col - 1]; 
-            const [left2X, left2Y] = [coins[COIN2].row, coins[COIN2].col - 1]; 
-            if (input[left1X][left1Y] !== '#') {
-                canMove1 = true;
-                coins[COIN1].col -= 1;
-            }
-            if (input[left2X][left2Y] !== '#') {
-                canMove2 = true;
-                coins[COIN2].col -= 1;
-            }
-            break;
-        case 'right':
-            const [right1X, right1Y] = [coins[COIN1].row, coins[COIN1].col + 1 ]; 
-            const [right2X, right2Y] = [coins[COIN2].row, coins[COIN2].col + 1 ]; 
-            if (input[right1X][right1Y] !== '#') {
-                canMove1 = true;
-                coins[COIN1].col += 1;
-            }
-            if (input[right2X][right2Y] !== '#') {
-                canMove2 = true;
-                coins[COIN2].col += 1;
-            }
-            break;
-        default:
-            break;
+    //3. 2번 동전만 밖에있는 경우
+    else if (
+      (nextR2 < 0 || nextR2 >= N || nextC2 < 0 || nextC2 >= M) &&
+      nextR1 >= 0 &&
+      nextR1 < N &&
+      nextC1 >= 0 &&
+      nextC1 < M
+    ) {
+      console.log(step + 1);
+      return;
     }
-
-        /** 상 */
-        dfs('up',count+1);
-        if(canMove1) coins[COIN1].row += 1;
-        if(canMove2) coins[COIN2].row += 1;
-        /** 하 */
-        dfs('down',count+1);
-        if(canMove1) coins[COIN1].row -= 1;
-        if(canMove2) coins[COIN2].row -= 1;
-        /** 좌 */
-        dfs('left',count+1);
-        if(canMove1) coins[COIN1].col += 1;
-        if(canMove2) coins[COIN2].col += 1;
-        
-        /** 우 */
-        dfs('right',count+1);
-        if(canMove1) coins[COIN1].col -= 1;
-        if(canMove2) coins[COIN2].col -= 1;
+    // 4. 1,2번 동전이 같이 밖에 나가는 경우
+    else if (
+      (nextR1 < 0 || nextR1 >= N || nextC1 < 0 || nextC1 >= M) &&
+      (nextR2 < 0 || nextR2 >= N || nextC2 < 0 || nextC2 >= M) &&
+      step + 1 > 10
+    ) {
+      console.log(-1);
+      return;
+    } else if (
+      (nextR1 < 0 || nextR1 >= N || nextC1 < 0 || nextC1 >= M) &&
+      (nextR2 < 0 || nextR2 >= N || nextC2 < 0 || nextC2 >= M)
+    )
+      continue;
+    //빈공간일경우
+    else if (matrix[nextR1][nextC1] === "." && matrix[nextR2][nextC2] === ".") {
+      queue.push([[nextR1, nextC1], [nextR2, nextC2], step + 1]);
+    }
+    //벽인경우
+    else if (matrix[nextR1][nextC1] === "#" && matrix[nextR2][nextC2] === ".") {
+      queue.push([[r1, c1], [nextR2, nextC2], step + 1]);
+    } else if (
+      matrix[nextR1][nextC1] === "." &&
+      matrix[nextR2][nextC2] === "#"
+    ) {
+      queue.push([[nextR1, nextC1], [r2, c2], step + 1]);
+    } else if (
+      matrix[nextR1][nextC1] === "#" &&
+      matrix[nextR2][nextC2] === "#" &&
+      step + 1 > 10
+    ) {
+      console.log(-1);
+      return;
+    } else if (matrix[nextR1][nextC1] === "#" && matrix[nextR2][nextC2] === "#")
+      continue;
+  }
 }
-
-dfs(null,0);
-
-console.log(answer);
-
-
-
-
+console.log(-1);
