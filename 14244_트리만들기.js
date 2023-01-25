3f93a91a0e2e17bffb9a470ed0a208dc5be8f66c
hyuntae <believe4279@gmail.com>
2023-01-25 13:34:29 +0900

    14244_트리만들기

---
+++ 14244_트리만들기.js
@@ -0,0 +1,16 @@
+const [N, M] = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const answer = [];
+
+for (let i = 0; i < M - 1; i++) {
+  answer.push(i + " " + (M - 1));
+}
+for (let i = M - 1; i < N - 1; i++) {
+  answer.push(i + " " + (i + 1));
+}
+console.log(answer.join("\n"));
